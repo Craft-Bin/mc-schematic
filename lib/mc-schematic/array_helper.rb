bb9856d6b70c46ac979058f8d65e1e102fd7f684
August <augustt198@gmail.com>
2014-05-06 09:28:20 -0400

    Add array helper for getting surface

--- lib/mc-schematic/array_helper.rb
+++ lib/mc-schematic/array_helper.rb
@@ -16,5 +16,27 @@ module MCSchematic
       end
       blocks
     end
+    # Gets the 'surface' of a multidimensional array
+    def get_surface(data)
+      blocks = []
+      for y in 0..(data.length - 1)
+        for x in 0..(data[y].length - 1) do
+          for z in 0..(data[y][x].length - 1) do
+            block = data[y][x][z]
+            if y == 0
+              blocks[x] = [] if blocks[x] == nil
+              blocks[x][z] = block
+              next
+            end
+            if block != 0
+              blocks[x] = [] if blocks[x] == nil
+              blocks[x][z] = block
+            end
+          end
+        end
+      end
+      blocks
+    end
+    
   end
 end
